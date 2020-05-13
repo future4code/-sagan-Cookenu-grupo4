aeb7654effb4d75f3404e2183ceac626f86a9e10
DennisAlves <dennis.alves@outlook.com>
2020-05-13 15:56:45 -0300

    Update cookene.sql
    correção dos relacionamentos

--- cookene.sql
+++ cookene.sql
@@ -6,12 +6,13 @@ CREATE TABLE cookenu_user (
     password_user VARCHAR (255) NOT NULL
 );
 CREATE TABLE cookenu_user_follow (
-	id_user_follow VARCHAR (255) NOT NULL PRIMARY KEY,
+	id_user_follow VARCHAR (255) NOT NULL,
     id_user_followed VARCHAR (255) NOT NULL,
     FOREIGN KEY (id_user_follow) REFERENCES cookenu_user(id_user)
 );
 CREATE TABLE cookenu_recipe(
-	id_recipe VARCHAR (255) NOT NULL PRIMARY KEY,
+	id_user_creator VARCHAR (255) NOT NULL,
+	id_recipe VARCHAR (255) NOT NULL,
     title_recipe VARCHAR (255) NOT NULL,
     description_recipe VARCHAR (255) NOT NULL,
     create_date_recipe DATE NOT NULL,
