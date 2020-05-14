6b8cb6ee2ebb00da63348223f42242f2bcb60288
Nauara Melo <nauaramelo@gmail.com>
2020-05-14 15:48:22 -0300

    Retornando o valor anterior pós testes

--- src/services/Authenticator.ts
+++ src/services/Authenticator.ts
@@ -1,7 +1,7 @@
 import * as jwt from "jsonwebtoken";
 
 export class Authenticator {
-    private static EXPIRES_IN = "15min";
+    private static EXPIRES_IN = "1min";
     public generateToken(input: AuthenticationData): string {
         const token = jwt.sign(
             {
